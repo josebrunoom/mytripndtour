b087abfa67140504363aacc39366a15fbbd9e0e5
Loas717 <133361796+Loas717@users.noreply.github.com>
2024-08-23 09:33:39 -0300

    new buttons on sidebar

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -5,6 +5,60 @@
                 <div class="flex justify-center mb-4 top-0">
                     <img src="../../assets/logo.jpg" alt="Logo" class="img-Logo" />
                 </div>
+                <div class="col-12 pb-3">
+                    <div v-if="currentRouteName=='home'">
+                        <button 
+                            type="button" 
+                            class="me-2 bg-[#78c0d6] text-white pl-2 pr-2 rounded-lg w-36" 
+                            @click=""
+                        >
+                        <i class="fa-regular fa-file"></i>
+                        Roteiros
+                        </button>
+                    </div>
+                    <div v-else>
+                        <button 
+                            type="button" 
+                            class="me-2 text-black pl-2 pr-2 rounded-lg w-36" 
+                            @click="goTo('home')"
+                        >
+                        <i class="fa-regular fa-file"></i>
+                        Roteiros
+                        </button>
+                    </div>
+            </div>
+            <div class="col-12 pb-3">
+                <div v-if="currentRouteName=='Perfil'">
+                    <button 
+                    type="button" 
+                    class= "me-2 bg-[#78c0d6] text-white pl-2 pr-2 rounded-lg w-36" 
+                    @click=""
+                    >
+                    <i class="fa-regular fa-user"></i>
+                    Meu Perfil
+                    </button>
+                </div>
+                <div v-else>
+                    <button 
+                    type="button" 
+                    class="me-2 text-black pl-2 pr-2 rounded-lg w-36" 
+                    @click=""
+                    >
+                    <i class="fa-regular fa-user"></i>
+                    Meu Perfil
+                    </button>
+                </div>
+            </div>
+            <div class="col-12 pb-3">
+                <button 
+                    type="button" 
+                    class="me-2 text-black hover:text-white pl-2 pr-2 rounded-lg w-36" 
+                    @click="postRoteiro"
+                >
+                <i class="fa-solid fa-door-open"></i>
+                Sair
+                </button>
+            </div>
             </div>
         </div>
         <div class="w-full bg-[#faf8fd]"> 
@@ -28,17 +82,29 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-import { RouterView } from 'vue-router';
+import { ref, onMounted,watchEffect  } from 'vue';
+import { RouterView, useRoute } from 'vue-router';
+import router from '../../routes';
 
 const user = JSON.parse(localStorage.getItem('user'));
 const name = ref('')
+const route = useRoute();
+const currentRouteName=ref(route.name)
 let img
+
 
 onMounted(() => {
     name.value=user.Nome
     img=user.photo
+    console.log(route.name);
 })
+watchEffect(() => {
+    currentRouteName.value = route.name;
+    console.log('Current route name:', currentRouteName.value);
+});
+const goTo=(id)=>{
+    router.push('/mytrip/'+id);
+}
 </script>
 
 <style scoped>
