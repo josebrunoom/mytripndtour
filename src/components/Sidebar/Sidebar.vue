2daae100983fe4fc2dbe071007d74e3564dd1304
Loas717 <133361796+Loas717@users.noreply.github.com>
2024-08-30 16:49:38 -0300

    lenguage select

--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -3,7 +3,7 @@
         <div class="w-full hidden sm:block md:w-[500px] lg:w-[300px] h-full mt-4 ml-4">
             <div class="h-[calc(100vh-50px)] bg-[#ffff] rounded-lg">
                 <div class="flex justify-center mb-4 top-0">
-                    <img src="../../assets/logo.jpg" alt="Logo" class="img-Logo" />
+                    <img src="../../assets/Logotipo/Arquivos em Imagem/Arquivos com Fundo/Logotipo_MyTripNTour_ComFundo-01.jpg" alt="Logo" class="img-Logo" />
                 </div>
                 <div class="col-12 pb-3">
                     <div v-if="currentRouteName=='home'">
@@ -53,7 +53,7 @@
                 <button 
                     type="button" 
                     class="me-2 text-black hover:text-white pl-2 pr-2 rounded-lg w-36" 
-                    @click="postRoteiro"
+                    @click=""
                 >
                 <i class="fa-solid fa-door-open"></i>
                 Sair
@@ -70,6 +70,7 @@
                     width: 10%;
                 "> -->
                 <div class="d-flex justify-content-end align-items-center mt-4 pr-16">
+                    <button class="fw-bold mr-4" @click="openDialog"><i class="fa-solid fa-globe"></i> Idioma</button>
                     <img class="inline-block h-8 w-8 rounded-full ring-2 ring-white me-2" :src="img" alt="" />
                     <button class="fw-bold">{{ name }}</button>
                 </div>
@@ -78,6 +79,22 @@
                 <RouterView></RouterView>
             </div>
         </div>
+        <v-dialog v-model="dialog" max-width="500px">
+      <v-card>
+        <v-card-title class="headline">Selecionar Idioma</v-card-title>
+        <v-card-text></v-card-text>
+            <select class="form-control " v-model="lenguage" @change="savelang">
+                <option value="" disabled>Selecione o Idioma</option>
+                <option value="EN-US">Inglês</option>
+                <option value="PT-BR">Português</option>
+                <option value="ES">Espanhol</option>
+            </select>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="[#78c0d6]" text @click="dialog=false">OK</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
     </div>
 </template>
 
@@ -91,8 +108,12 @@ const name = ref('')
 const route = useRoute();
 const currentRouteName=ref(route.name)
 let img
+const dialog=ref(false)
+const lenguage = ref('')
 
-
+const savelang = () => {
+    localStorage.setItem('lang', lenguage.value)
+}
 onMounted(() => {
     name.value=user.Nome
     img=user.photo
@@ -104,6 +125,9 @@ watchEffect(() => {
 });
 const goTo=(id)=>{
     router.push('/mytrip/'+id);
+}
+const openDialog = () =>{
+    dialog.value = true;
 }
 </script>
 
