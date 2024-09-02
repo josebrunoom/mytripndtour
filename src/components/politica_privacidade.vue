557c69295580db8d85c37a4ae3c4503ba735cefe
Loas717 <133361796+Loas717@users.noreply.github.com>
2024-09-02 16:56:44 -0300

    politica de privacidade

---
+++ src/components/politica_privacidade.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <div v-html="htmlContent"></div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  import axios from 'axios';
+  
+  const htmlContent = ref('');
+  
+  onMounted(async () => {
+    console.log('afsasf')
+    try {
+      // Load the HTML file from the assets folder
+      const response = await axios.get(new URL('../assets/MTT_Política_de_Privacidade.htm', import.meta.url).href);
+      htmlContent.value = response.data;
+    } catch (error) {
+      console.error('Error loading HTML content:', error);
+    }
+  });
+  </script>
