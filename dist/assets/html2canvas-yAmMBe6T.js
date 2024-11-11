5d90870e1186eb76ceda56907c7a8d682fc2858c
Loas717 <133361796+Loas717@users.noreply.github.com>
2024-11-11 12:44:06 -0300

    new build

--- dist/assets/html2canvas-yAmMBe6T.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:dist/assets/html2canvas-D-IdMw0X.js
-import{g as c,r as f}from"./index-RCJzpru4.js";function l(r,n){for(var o=0;o<n.length;o++){const e=n[o];if(typeof e!="string"&&!Array.isArray(e)){for(const t in e)if(t!=="default"&&!(t in r)){const a=Object.getOwnPropertyDescriptor(e,t);a&&Object.defineProperty(r,t,a.get?a:{enumerable:!0,get:()=>e[t]})}}}return Object.freeze(Object.defineProperty(r,Symbol.toStringTag,{value:"Module"}))}var s=f();const i=c(s),u=l({__proto__:null,default:i},[s]);export{u as h};
-========
-import{g as c,r as f}from"./index-BXjH2RB6.js";function l(r,n){for(var o=0;o<n.length;o++){const e=n[o];if(typeof e!="string"&&!Array.isArray(e)){for(const t in e)if(t!=="default"&&!(t in r)){const a=Object.getOwnPropertyDescriptor(e,t);a&&Object.defineProperty(r,t,a.get?a:{enumerable:!0,get:()=>e[t]})}}}return Object.freeze(Object.defineProperty(r,Symbol.toStringTag,{value:"Module"}))}var s=f();const i=c(s),u=l({__proto__:null,default:i},[s]);export{u as h};
->>>>>>>> 5008e4d4573d2a87375b85aff78b681f5711ea7b:dist/assets/html2canvas-yAmMBe6T.js
