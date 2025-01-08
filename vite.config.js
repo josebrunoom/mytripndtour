a9876225434b6ac26f1974a669a2dec89099211d
Loas717 <133361796+Loas717@users.noreply.github.com>
2025-01-08 16:26:59 -0300

    /roteiro

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: '/roteiro/',
   /* server: {
     https: {
       key: fs.readFileSync(path.resolve(__dirname, 'localhost+2-key.pem')),
