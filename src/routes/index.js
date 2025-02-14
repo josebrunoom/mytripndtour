a15fe4f642f7c390f9c9a53cbe01ba6190c2ecf6
josebrunoom <josebrunoom@gmail.com>
2025-02-14 08:49:40 -0300

    change index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ import { createWebHistory, createRouter } from 'vue-router'
 import routes from './routes';
 
 const router = createRouter({
-    history: createWebHistory("/roteiro/"),
+    history: createWebHistory(""),
     routes,
   })
 
